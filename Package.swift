922692f788ef4fc351a8e04de88aeb2e034c30e4
Tom Bell <tomb@tombell.dev>
2026-09-05 16:55:38 +0100

    Update Package.swift to target swift 6.2

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 import PackageDescription
 
 let package = Package(
   name: "sbar",
-  platforms: [.macOS(.v14)],
-  products: [.executable(name: "sbar", targets: ["Sbar"])],
+  platforms: [
+    .macOS(.v26)
+  ],
+  products: [
+    .executable(name: "sbar", targets: ["Sbar"])
+  ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.1")
   ],
@@ -15,7 +19,10 @@ let package = Package(
         "SbarCore", .product(name: "ArgumentParser", package: "swift-argument-parser"),
       ]
     ),
-    .target(name: "SbarCore", resources: [.copy("Resources/config.schema.json")]),
+    .target(
+      name: "SbarCore",
+      resources: [.copy("Resources/config.schema.json")]
+    ),
     .testTarget(name: "SbarTests", dependencies: ["Sbar"]),
     .testTarget(name: "SbarCoreTests", dependencies: ["SbarCore"]),
   ]
