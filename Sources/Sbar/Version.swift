ca68ae83265bccbe0d850b967313d52487bb05ff
Tom Bell <tomb@tombell.dev>
2026-09-15 21:12:07 +0100

    Bump version to v0.0.1

--- Sources/Sbar/Version.swift
+++ Sources/Sbar/Version.swift
@@ -1,7 +1,7 @@
 /// Build version embedded in the sbar executable.
 struct Version {
   /// Current generated version.
-  static let current = Self(value: "v0.0.0")
+  static let current = Self(value: "v0.0.1")
 
   /// Version string printed by `sbar --version`.
   let value: String
